05162f764cf26f40cb73c25ca73c77bb35b9b748
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-05-04 20:45:31 +0000

    Add OpenAI API integration
    This commit adds a new OpenAi class to the LaravelIntelliDb namespace that allows for executing the OpenAI API with a given prompt. The execute function takes in a prompt and returns the completed message as a JSON array. The function also supports a maxTokens parameter to limit the number of tokens returned.

---
+++ src/OpenAi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Salehhashemi\LaravelIntelliDb;
+
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
+
+class OpenAi
+{
+    /**
+     * Execute the OpenAI API call with a given prompt.
+     *
+     * @throws RequestException
+     */
+    public function execute(string $prompt, int $maxTokens = 300): array
+    {
+        $input_data = [
+            'temperature' => 0.7,
+            'max_tokens' => $maxTokens,
+            'frequency_penalty' => 0,
+            'model' => config('intelli-db.model'),
+            'messages' => [
+                [
+                    'role' => 'user',
+                    'content' => $prompt,
+                ],
+            ],
+        ];
+
+        $response = Http::asForm()->withHeaders([
+            'Authorization' => 'Bearer '.config('intelli-db.openAiApiKey'),
+            'Content-Type' => 'application/json',
+        ])->post('https://api.openai.com/v1/chat/completions', $input_data);
+
+        if ($response->successful()) {
+            $complete = $response->json();
+
+            return $complete['choices'][0]['message']['content'];
+        } else {
+            throw new RequestException($response);
+        }
+    }
+}
