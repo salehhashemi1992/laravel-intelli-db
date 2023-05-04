d5758b4a578a65dfb238b2ff99ba3a1cddc1f49c
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-05-04 20:34:13 +0000

    Move Intelli DB config file to a new directory
    The Intelli DB config file was moved from the .github/config directory to the config directory. The file path was updated in the code accordingly, and the file was also modified to include two new configurations for the openAiApiKey and model.

--- .github/config/intelli-db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*
- * Intelli DB configs
- */
-return [
-
-];
---
+++ config/intelli-db.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Intelli DB configs
+ */
+return [
+    'openAiApiKey' => env('OPEN_AI_KEY'),
+    'model' => env('MODEL', 'gpt-3.5-turbo')
+];
