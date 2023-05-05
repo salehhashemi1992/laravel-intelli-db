fb716a47c34b2d4a01cc07788fed8d6243114a71
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-05-05 11:20:26 +0000

    Refactor ExtendedRuleMakeCommandConsoleTest to use Orchestra Testbench
    In this commit, the ExtendedRuleMakeCommandConsoleTest file was refactored to use Orchestra Testbench. The LaravelIntelliDbServiceProvider was removed from the file and the custom command was registered using a singleton in the setUp() method. Tests were added to ensure that the rule is created with and without the "ai" option and description.

--- tests/ExtendedRuleMakeCommandConsoleTest.php
+++ tests/ExtendedRuleMakeCommandConsoleTest.php
@@ -1,21 +1,48 @@
 <?php
 
-use Salehhashemi\LaravelIntelliDb\LaravelIntelliDbServiceProvider;
-use Salehhashemi\LaravelIntelliDb\Tests\ConsoleTestCase;
+namespace Salehhashemi\LaravelIntelliDb\Tests;
 
-class ExtendedRuleMakeCommandConsoleTest extends ConsoleTestCase
+use Illuminate\Support\Facades\File;
+use Orchestra\Testbench\TestCase;
+use Salehhashemi\LaravelIntelliDb\Console\ExtendedRuleMakeCommand;
+
+class ExtendedRuleMakeCommandConsoleTest extends TestCase
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected function getPackageProviders($app): array
+    protected function setUp(): void
     {
-        return [LaravelIntelliDbServiceProvider::class];
+        parent::setUp();
+
+        // Register the custom command
+        $this->app->singleton('command.rule.make', function ($app) {
+            return new ExtendedRuleMakeCommand($app['files']);
+        });
+        $this->app->booted(function () {
+            $this->app['command.rule.make']->setLaravel($this->app);
+        });
     }
 
     /** @test */
-    public function test_example()
+    public function test_creates_a_rule_without_ai_option()
     {
-        $this->assertTrue(true);
+        $this->artisan('make:rule', [
+            'name' => 'SampleRule',
+        ])->assertExitCode(0);
+
+        // Check if the rule file has been created
+        $this->assertTrue(File::exists(app_path('Rules/SampleRule.php')));
+    }
+
+    /** @test */
+    public function test_creates_a_rule_with_ai_option_and_description()
+    {
+        $description = 'validate unique email';
+
+        $this->artisan('make:rule', [
+            'name' => 'SampleRule',
+            '--ai' => true,
+            '--description' => $description,
+        ])->assertExitCode(0);
+
+        $this->assertTrue(file_exists(app_path('Rules/SampleRule.php')));
     }
 }
